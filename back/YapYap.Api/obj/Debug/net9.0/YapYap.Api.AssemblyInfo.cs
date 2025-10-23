5e99abc59e3341b86264ff851d10f7917f3aebdb
RaphaelAZ <azevedo.raphael2017@gmail.com>
2025-10-23 20:03:51 +0200

    (~) fix

--- back/YapYap.Api/obj/Debug/net9.0/YapYap.Api.AssemblyInfo.cs
+++ back/YapYap.Api/obj/Debug/net9.0/YapYap.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YapYap.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b3e3eb89afb26bbf2fb7cbdace54f1ad3565f20")]
 [assembly: System.Reflection.AssemblyProductAttribute("YapYap.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YapYap.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
